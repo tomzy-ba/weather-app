301b3a249c6c22152727b9301a717e157ba36ce3
tomzy-ba <trbee100@gmail.com>
2024-02-18 16:09:59 +0000

    added loading inidcator

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 import APIKEY from "./APIKEY.js";
 
-let locationVar = "New York";
 async function getWeather(locationVar) {
+    document.querySelector("#loading").classList.remove("hidden")
+    await new Promise(resolve => setTimeout(resolve, 2000)); // Simulate 2 seconds delay
+    document.querySelector("#loading").classList.add("hidden")
     const weatherJson = await fetch(`https://api.weatherapi.com/v1/current.json?key=${APIKEY}&q=${locationVar}`);
     let weatherData = await weatherJson.json();
-    console.log(weatherData)
 
     const weatherDiv = document.querySelector("#weatherDiv");
     const tempSpan = document.querySelector("#temp");
@@ -15,10 +16,11 @@ async function getWeather(locationVar) {
     tempSpan.innerText = weatherData.current.temp_c;
     status.innerText = weatherData.current.condition.text;
     windSpeed.innerText = weatherData.current.wind_mph + "mph"
-}
+
+};
 
 document.querySelector("form").addEventListener("submit", (event) => {
     event.preventDefault();
     const locationInput = document.querySelector("#locationInput");
-    getWeather(locationInput.value);
+    getWeather(locationInput.value)
 })
